584612a9730dd802ac405c9f4873b669e7657766
Wojciech Galecki <wojciech.galecki@wp.pl>
2025-01-09 13:40:55 +0100

    updating data

--- src/components/util/http.js
+++ src/components/util/http.js
@@ -1,5 +1,11 @@
+import { QueryClient } from "@tanstack/react-query";
+
+const BASE_URL = "http://localhost:3000/events";
+
+export const queryClient = new QueryClient();
+
 export async function fetchEvents({ signal, searchTerm }) {
-  let url = "http://localhost:3000/events";
+  let url = BASE_URL;
 
   if (searchTerm) {
     url += `?search=${searchTerm}`;
@@ -17,4 +23,40 @@ export async function fetchEvents({ signal, searchTerm }) {
   const { events } = await response.json();
 
   return events;
+}
+
+export async function createNewEvent(eventData) {
+  const response = await fetch(BASE_URL, {
+    method: "POST",
+    body: JSON.stringify(eventData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while creating the event");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { event } = await response.json();
+
+  return event;
+}
+
+export async function fetchSelectableImages({ signal }) {
+  const response = await fetch(`${BASE_URL}/images`, { signal });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the images");
+    error.code = response.status;
+    error.info = await response.json();
+    throw error;
+  }
+
+  const { images } = await response.json();
+
+  return images;
 }
